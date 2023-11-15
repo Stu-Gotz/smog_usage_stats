6096f91baaad0b8ae49caf5bc977a21da59fca75
Alan Nardo <alan.nardo.91@gmail.com>
2023-11-15 02:28:07 +0000

    started but did not finish readme.md

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+
+@pytest.fixture(autouse=True)
+def disable_network_calls(monkeypatch):
+    def stunted_get():
+        raise RuntimeError("Network access not allowed during testing!")
+    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
